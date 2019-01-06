ce51a086d2c2b9e4f4038990d8b0f2dcd3bd1010
m0nkeykong <haim1321@gmail.com>
2019-01-06 03:30:07 +0200

    first view done

--- assets/js/secondView.js
+++ assets/js/secondView.js
@@ -1,6 +1,6 @@
 $("#ldview2").click(() => {
     //get data
-    var dataPoints = [];
+    var dps = new Array();
     var request = new XMLHttpRequest();
     request.open('GET', 'https://wineandweather.herokuapp.com/api/AlcoholVolsEachCountry', true);
     request.onload = function () {
@@ -8,7 +8,6 @@ $("#ldview2").click(() => {
         var data = JSON.parse(this.response);
         if (request.status >= 200 && request.status < 400) {
             for (var k in data){
-                //console.log(data[k][0].Price)
                 var maxPrice = 0.0, maxAlcohol = 0.0, minPrice = 0.0, minAlcohol = 0.0;
                 for(var i = 0; i < data[k].length; ++i){
                     //console.log(data[k][i].Alcohol)
@@ -17,35 +16,35 @@ $("#ldview2").click(() => {
                     }
                     if(maxAlcohol < data[k][i].Alcohol) maxAlcohol = data[k][i].Alcohol;
                 }
-                //console.log('alcohol: ' + maxAlcohol + '   max price: ' + maxPrice + '   min price: ' + minPrice);
                 minPrice = data[k].map(function(el){return el.Price}).reduce(function(el){return Math.min(el)});     //get min price
                 minAlcohol = data[k].map(function(el){return el.Alcohol}).reduce(function(el){return Math.min(el)});     //get min alcohol
-                dataPoints.push({y: [minAlcohol, parseFloat(maxAlcohol)], label: k, price: [minPrice, parseFloat(maxPrice)]});
-            }
+                dps.push({y: [minAlcohol, parseFloat(maxAlcohol)], label: k, price: [minPrice, parseFloat(maxPrice)]});
+            }      
+            console.log(dps.length);   
+            var chart = new CanvasJS.Chart("chartContainer", {
+                animationEnabled: true,
+                theme: "light2",
+                axisY: {
+                    includeZero: false,
+                    title: "Alcohol/Lit",
+                    crosshair: {
+                        enabled: true
+                    },
+                    gridColor: "white"
+                },
+                data: [{
+                    fillOpacity: .7,
+                    type: "rangeBar",
+                    zzzzindexLabel: "{y[#index]}%",
+                    toolTipContent: "<b>Country</b>: {label} <br> <b>Alcohol Precent</b>: {y[0]}% - {y[1]}%<br> <b>Price Range</b>: {price[0]}$ - {price[1]}$",
+                    dataPoints: dps
+                }]
+            });
+            chart.render();
+                            
         } else {
-                console.log(`Gah, it's not working!`);
+            console.log(`Gah, it's not working!`);
         }
     }
-    request.send();
-    console.log(dataPoints)
-    var chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled: true,
-        theme: "light2",
-        axisY: {
-            includeZero: false,
-            title: "Alcohol/Lit",
-            crosshair: {
-                enabled: true
-            },
-            gridColor: "white"
-        },
-        data: [{
-            fillOpacity: .7,
-            type: "rangeBar",
-            indexLabel: "{y[#index]}%",
-            toolTipContent: "<b>Country</b>: {label} <br> <b>Alcohol Precent</b>: {y[0]}% - {y[1]}%<br> <b>Price Range</b>: {price[0]}$ - {price[1]}$",
-            dataPoints: dataPoints
-        }]
-    });
-    chart.render();
+    request.send()
 });
